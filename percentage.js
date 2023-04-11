0b6a0df1d71d763888f79229937006f81fb7351d
Ziaenso_ <122286711+Ziaensodayo@users.noreply.github.com>
2023-04-11 16:21:52 +0900

    Update percentage.js

--- percentage.js
+++ percentage.js
@@ -7,7 +7,7 @@ function displayTime() {
     const timeString = `${hours}:${minutes}:${seconds}.${minseconds}`;
     const years = now.getFullYear().toString();
     const months = now.getMonth().toString().padStart(2,"0");
-    const days = now.getDay().toString();
+    const days = now.getDate().toString().padStart(2,"0");
     const dayString = `${years}/${months}/${days}`
     document.getElementById("time").textContent = timeString;
     document.getElementById("day").textContent = dayString;
